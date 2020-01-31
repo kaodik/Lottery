53a431feea288362bf88fe1ba264522fa83bc2be
kaodik <52609996+kaodik@users.noreply.github.com>
2020-01-31 12:14:16 -0600

    Update BinaryIO.java
    removed some comments

--- src/binaryio/BinaryIO.java
+++ src/binaryio/BinaryIO.java
@@ -1,15 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package binaryio;
 import java.io.*;
 import java.util.Scanner;
-/**
- *
- * @author T951
- */
+
+
 public class BinaryIO {
 
     /**
